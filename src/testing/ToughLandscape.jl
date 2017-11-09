9c0f145650c229440eb04a24e33c09791f235b2a
Jacob <jacob_ruth@brown.edu>
2017-11-09 12:29:01 -0500

    toy function

--- src/testing/ToughLandscape.jl
+++ src/testing/ToughLandscape.jl
@@ -1,24 +1,25 @@
 using PTChain
 
-# Since the INSSwap algorithm is going to assign temperatures based on energy scores,
-# it will do poorly when having a high barrier around the lowest point.
 function score_vec(vec)
     score = 0
     for i = 1:length(vec)
-        if abs(vec[i]) <= 0.25
-            score += 0 #don't really need
-        elseif abs(vec[i]) <= 0.5
-            score += 4
-        elseif abs(vec[i]) < 1
-            score += 2
-        elseif abs(vec[i]) < 1.25
-            score += 4
+        if abs(vec[i]) <= 4
+            score += abs(vec[i])
+        elseif abs(vec[i]) <= 6
+            score += 8 - abs(vec[i])
         else
-            score += 0.25*vec[i]^2
+            score += abs(vec[i]) - 4
         end
-
     end
-score
+    score
+end
+
+function random_walk(x::Vector{Float64})
+    n = length(x)
+    direction = rand(1:n)
+    y = copy(x)
+    y[direction] = y[direction] + rand(-1:2:1)
+    return y
 end
 
 function widescaleRandomNoiseTL(x::Vector{Float64})
@@ -43,8 +44,27 @@ function annealSolverTL(dims)
     end
     min_val, min_x
 end
+
 function insSolver(dims)
     start = randn(dims)
     reps = 30
-    sampleTempering(start, score_vec, [0, 4, 10], widescaleRandomNoiseTL, reps, 50)
+    sampleTempering(start, score_vec, [0, 4, 10], random_walk, reps, 50)
+end
+
+#calculates the expected number of steps from a position on the function to a max
+#steps up the recurrance that F(x) = p*(F(x+1) + 1) + (1 - p)*(F(x - 1) + 1)
+function rand_walk_solver(steps, temp, ΔF=1)
+    p = exp(-ΔF/temp)/2
+    vec = ones(steps)
+    vec[end] = 0
+    mat = zeros(steps, steps)
+    mat[1, 1] = p
+    mat[1, 2] = -p
+    for  i = 2:(steps - 1)
+        mat[i, i - 1] = -(1 - p)
+        mat[i, i] = 1
+        mat[i, i + 1] = -p
+    end
+    mat[steps, steps] = 1
+    return inv(mat)*vec
 end
